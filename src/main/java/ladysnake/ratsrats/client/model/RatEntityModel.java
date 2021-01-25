639770f3834c2e5d09e6dc6ee9519ee8182f21f1
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-01-25 16:47:50 +0100

    Add head pitch

--- src/main/java/ladysnake/ratsrats/client/model/RatEntityModel.java
+++ src/main/java/ladysnake/ratsrats/client/model/RatEntityModel.java
@@ -2,8 +2,13 @@ package ladysnake.ratsrats.client.model;
 
 import ladysnake.ratsrats.common.Rats;
 import ladysnake.ratsrats.common.entity.RatEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class RatEntityModel extends AnimatedGeoModel<RatEntity> {
     @Override
@@ -19,5 +24,17 @@ public class RatEntityModel extends AnimatedGeoModel<RatEntity> {
     @Override
     public Identifier getAnimationFileLocation(RatEntity object) {
         return new Identifier(Rats.MODID, "animations/entity/rat.animation.json");
+    }
+
+    @Override
+    public void setLivingAnimations(RatEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+        IBone body = this.getAnimationProcessor().getBone("body");
+
+        if (head != null) {
+            head.setRotationX(-entity.pitch * ((float) Math.PI / 180F));
+//            head.setRotationY(entity.getHeadYaw() * ((float) Math.PI / 180F));
+        }
     }
 }
