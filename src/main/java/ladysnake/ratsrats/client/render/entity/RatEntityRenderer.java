f6b10d62ff994d97af8bb87825ded9b6b0777662
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-02-08 14:07:25 +0100

    Make shadow puppet rat glow in the dark

--- src/main/java/ladysnake/ratsrats/client/render/entity/RatEntityRenderer.java
+++ src/main/java/ladysnake/ratsrats/client/render/entity/RatEntityRenderer.java
@@ -12,7 +12,9 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Quaternion;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderer.geo.GeoEntityRenderer;
@@ -55,5 +57,14 @@ public class RatEntityRenderer extends GeoEntityRenderer<RatEntity> {
         }
 
         super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+    }
+
+    @Override
+    protected int getBlockLight(RatEntity rat, BlockPos blockPos) {
+        if (rat.getRatType() == RatEntity.Type.RAT_KID && rat.getRatColor() == DyeColor.PURPLE) {
+            return 15;
+        } else {
+            return super.getBlockLight(rat, blockPos);
+        }
     }
 }
