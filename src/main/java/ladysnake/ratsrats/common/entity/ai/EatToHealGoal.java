8bcb6d447e73a0cbcd1e81f967ebfb86146d30b3
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-02-11 00:13:18 +0100

    Eat any food

--- src/main/java/ladysnake/ratsrats/common/entity/ai/EatToHealGoal.java
+++ src/main/java/ladysnake/ratsrats/common/entity/ai/EatToHealGoal.java
@@ -3,11 +3,13 @@ package ladysnake.ratsrats.common.entity.ai;
 import ladysnake.ratsrats.common.entity.RatEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.PotionItem;
 import net.minecraft.particle.ItemStackParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.UseAction;
 
 public class EatToHealGoal extends Goal {
     public final RatEntity rat;
@@ -19,7 +21,7 @@ public class EatToHealGoal extends Goal {
 
     @Override
     public boolean canStart() {
-        return this.rat.isBreedingItem(this.rat.getMainHandStack()) && this.rat.getHealth() < this.rat.getMaxHealth() && !this.rat.getMoveControl().isMoving();
+        return (this.rat.isBreedingItem(this.rat.getMainHandStack()) || this.rat.getMainHandStack().getItem() instanceof PotionItem) && this.rat.getHealth() < this.rat.getMaxHealth() && !this.rat.getMoveControl().isMoving();
     }
 
     @Override
@@ -31,22 +33,25 @@ public class EatToHealGoal extends Goal {
                 eatingTicks--;
 
                 if (this.eatingTicks % 4 == 0 && this.eatingTicks > 0) {
-                    if (this.rat.getMainHandStack().isFood()) {
-                        ((ServerWorld) this.rat.world).spawnParticles(new ItemStackParticleEffect(ParticleTypes.ITEM, this.rat.getMainHandStack()), this.rat.getX() + this.rat.getRandom().nextGaussian() / 20f, this.rat.getY() + this.rat.getRandom().nextGaussian() / 20f, this.rat.getZ() + this.rat.getRandom().nextGaussian() / 20f, 5, this.rat.getRandom().nextGaussian() / 20f, 0.2D + this.rat.getRandom().nextGaussian() / 20f, this.rat.getRandom().nextGaussian() / 20f, 0.1f);
-                        ((ServerWorld) this.rat.world).playSoundFromEntity(null, this.rat, SoundEvents.ENTITY_GENERIC_EAT, SoundCategory.NEUTRAL, 0.01F, this.rat.getRandom().nextFloat() * 0.4F + 0.8F);
+                    if (this.rat.getMainHandStack().getItem().getUseAction(this.rat.getMainHandStack()) == UseAction.DRINK) {
+                        ((ServerWorld) this.rat.world).playSoundFromEntity(null, this.rat, this.rat.getMainHandStack().getItem().getDrinkSound(), SoundCategory.NEUTRAL, 0.01F, this.rat.getRandom().nextFloat() * 0.4F + 0.8F);
+                    } else if (this.rat.getMainHandStack().getItem().getUseAction(this.rat.getMainHandStack()) == UseAction.EAT) {
+                        ((ServerWorld) this.rat.world).spawnParticles(new ItemStackParticleEffect(ParticleTypes.ITEM, this.rat.getMainHandStack()), this.rat.getX() + this.rat.getRandom().nextGaussian() / 20f, this.rat.getY() + this.rat.getRandom().nextGaussian() / 20f, this.rat.getZ() + this.rat.getRandom().nextGaussian() / 20f, 5, this.rat.getRandom().nextGaussian() / 20f, 0.2D + this.rat.getRandom().nextGaussian() / 20f, this.rat.getRandom().nextGaussian() / 20f, 0.025f);
+                        ((ServerWorld) this.rat.world).playSoundFromEntity(null, this.rat, this.rat.getMainHandStack().getItem().getEatSound(), SoundCategory.NEUTRAL, 0.01F, this.rat.getRandom().nextFloat() * 0.4F + 0.8F);
                     }
                 }
 
                 if (this.eatingTicks <= 0) {
-                    if (this.rat.getMainHandStack().isFood()) {
 
+                    if (this.rat.getMainHandStack().getItem().isFood()) {
                         this.rat.heal((float) this.rat.getMainHandStack().getItem().getFoodComponent().getHunger());
                         this.rat.getMainHandStack().getItem().getFoodComponent().getStatusEffects().forEach(statusEffectInstanceFloatPair -> {
                             this.rat.addStatusEffect(statusEffectInstanceFloatPair.getFirst());
                         });
-                        this.rat.getMainHandStack().decrement(1);
-                        this.rat.setEating(false);
                     }
+                    this.rat.setEating(false);
+                    this.rat.getMainHandStack().getItem().finishUsing(this.rat.getMainHandStack(), this.rat.world, this.rat);
+
                 }
             } else {
                 this.rat.setEating(false);
