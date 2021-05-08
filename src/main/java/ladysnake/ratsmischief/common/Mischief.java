80530ef9f111350d8bf27fb0ed3a75c0b8df3c3c
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-05-08 16:37:00 +0200

    Finalize ratify command

--- src/main/java/ladysnake/ratsmischief/common/Mischief.java
+++ src/main/java/ladysnake/ratsmischief/common/Mischief.java
@@ -15,6 +15,8 @@ import ladysnake.requiem.api.v1.internal.StatusEffectReapplicator;
 import ladysnake.requiem.api.v1.possession.PossessionComponent;
 import ladysnake.requiem.api.v1.remnant.AttritionFocus;
 import ladysnake.requiem.api.v1.remnant.RemnantComponent;
+import ladysnake.requiem.api.v1.remnant.RemnantState;
+import ladysnake.requiem.api.v1.remnant.RemnantType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -84,12 +86,29 @@ public class Mischief implements ModInitializer {
 
             // kill soul players that are supposed to be rats cause being a rat is so much better than trying to find a new body requiem is cool look I love requiem but being a rat is so much better michael it's your birthday today
             server.getPlayerManager().getPlayerList().forEach(serverPlayerEntity -> {
-                if (PlayerRatComponent.KEY.get(serverPlayerEntity).isRat && RemnantComponent.get(serverPlayerEntity).isIncorporeal() && serverPlayerEntity.age > 20) {
-                    serverPlayerEntity.kill(); // haha get fucked nerd
+                if (PlayerRatComponent.KEY.get(serverPlayerEntity).isRat) {
+                    // turn into a remnant else they won't be able to possess rats
+                    if (RemnantComponent.get(serverPlayerEntity).getRemnantType() != (RemnantType) Registry.REGISTRIES.get(new Identifier("requiem:remnant_states")).get(new Identifier("requiem:remnant"))) {
+                        RemnantComponent.get(serverPlayerEntity).become((RemnantType) Registry.REGISTRIES.get(new Identifier("requiem:remnant_states")).get(new Identifier("requiem:remnant")));
+                    }
+
+                    // make them respawn on death, or kill them if they're humans
+                    if ((RemnantComponent.get(serverPlayerEntity).isIncorporeal() || !RemnantComponent.get(serverPlayerEntity).isVagrant()) && serverPlayerEntity.age > 20) {
+                        serverPlayerEntity.kill(); // haha get fucked nerd
+                    }
+
+                    if (PossessionComponent.get(serverPlayerEntity).getPossessedEntity() instanceof RatEntity) {
+                        if (serverPlayerEntity.isUsingItem()) {
+                            ((RatEntity) PossessionComponent.get(serverPlayerEntity).getPossessedEntity()).setEating(true);
+                        } else {
+                            ((RatEntity) PossessionComponent.get(serverPlayerEntity).getPossessedEntity()).setEating(false);
+                        }
+                    }
                 }
             });
         });
 
+        // remove attrition when respawning
         PlayerRespawnCallback.EVENT.register((serverPlayerEntity, returnFromEnd) -> {
             if (PlayerRatComponent.KEY.get(serverPlayerEntity).isRat && !returnFromEnd) {
                 serverPlayerEntity.clearStatusEffects();
@@ -101,6 +120,7 @@ public class Mischief implements ModInitializer {
             }
         });
 
+        // allow rat possession for rat players
         PossessionStartCallback.EVENT.register(new Identifier("ratsmischief:possession"), (mobEntity, playerEntity, b) -> {
             if (mobEntity instanceof RatEntity && PlayerRatComponent.KEY.get(playerEntity).isRat) {
                 return PossessionStartCallback.Result.ALLOW;
