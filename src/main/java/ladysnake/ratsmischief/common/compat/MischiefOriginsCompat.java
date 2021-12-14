b0cd3c049b9d486484b0ede9c973488c624ef9dd
Pyrofab <redstoneinfire@gmail.com>
2021-12-14 16:37:37 +0100

    Fix requiem compatibility

--- src/main/java/ladysnake/ratsmischief/common/compat/MischiefOriginsCompat.java
+++ src/main/java/ladysnake/ratsmischief/common/compat/MischiefOriginsCompat.java
@@ -5,6 +5,9 @@ import io.github.apace100.apoli.registry.ApoliRegistries;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import ladysnake.ratsmischief.common.Mischief;
+import ladysnake.requiem.common.gamerule.RequiemSyncedGamerules;
+import ladysnake.requiem.common.gamerule.StartingRemnantType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,10 +15,10 @@ import net.minecraft.util.registry.Registry;
 public class MischiefOriginsCompat {
 	public static final Identifier CONDITION_ID = new Identifier(Mischief.MODID, "enable_rat");
 	public static final ConditionFactory<Entity> CONDITION_FACTORY = new ConditionFactory<>(CONDITION_ID, new SerializableData().add("enabled", SerializableDataTypes.BOOLEAN), (instance, entity) -> {
-//		if (FabricLoader.getInstance().isModLoaded("requiem")) {
-//			StartingRemnantType startingRemnantType = RequiemSyncedGamerules.get(entity.world).getStartingRemnantType();
-//			return startingRemnantType.getRemnantType() == StartingRemnantType.CHOOSE.getRemnantType();
-//		}
+		if (FabricLoader.getInstance().isModLoaded("requiem")) {
+			StartingRemnantType startingRemnantType = RequiemSyncedGamerules.get(entity.world).getStartingRemnantType();
+			return startingRemnantType.getRemnantType() == StartingRemnantType.CHOOSE.getRemnantType();
+		}
 		return false;
 	});
 
