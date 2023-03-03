0f0151354b83775bdce3631557604715453b7c84
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-03-03 17:21:36 +0000

    staffs but actually

--- src/main/java/doctor4t/ratsmischief/common/item/RatStaffItem.java
+++ src/main/java/doctor4t/ratsmischief/common/item/RatStaffItem.java
@@ -4,8 +4,6 @@ import doctor4t.ratsmischief.common.entity.RatEntity;
 import doctor4t.ratsmischief.common.entity.ai.BreedGoal;
 import doctor4t.ratsmischief.common.entity.ai.DigGoal;
 import doctor4t.ratsmischief.common.entity.ai.HarvestPlantMealGoal;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.TargetGoal;
 import net.minecraft.entity.mob.HostileEntity;
@@ -14,16 +12,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import xyz.amymialee.mialeemisc.items.ICustomAttackItem;
+import xyz.amymialee.mialeemisc.items.IClickConsumingItem;
 import xyz.amymialee.mialeemisc.util.MialeeMath;
 
 import java.util.List;
 
-public class RatStaffItem extends Item implements ICustomAttackItem {
+public class RatStaffItem extends Item implements IClickConsumingItem {
 	public RatStaffItem(Settings settings) {
 		super(settings);
 	}
@@ -62,11 +61,10 @@ public class RatStaffItem extends Item implements ICustomAttackItem {
 	}
 
 	@Override
-	public boolean mialeeMisc$customAttack(LivingEntity livingEntity, Entity entity) {
-		ItemStack stack = livingEntity.getMainHandStack();
+	public void mialeeMisc$doAttack(ServerPlayerEntity serverPlayerEntity) {
+		ItemStack stack = serverPlayerEntity.getMainHandStack();
 		NbtCompound compound = stack.getOrCreateNbt();
-		compound.putInt("action", MialeeMath.clampLoop(compound.getInt("action") + (livingEntity.isSneaking() ? -1 : 1), 0, Action.values().length));
-		return false;
+		compound.putInt("action", MialeeMath.clampLoop(compound.getInt("action") + (serverPlayerEntity.isSneaking() ? -1 : 1), 0, Action.values().length));
 	}
 
 	public static Action getAction(ItemStack stack) {
