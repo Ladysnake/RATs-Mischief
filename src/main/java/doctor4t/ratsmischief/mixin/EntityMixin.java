abaf99c6edda6efa00b63b26cbc50dd90734ea70
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-03-03 22:04:38 +0000

    rats

---
+++ src/main/java/doctor4t/ratsmischief/mixin/EntityMixin.java
@@ -0,0 +1,23 @@
+package doctor4t.ratsmischief.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import doctor4t.ratsmischief.common.entity.RatEntity;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+	@WrapOperation(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;onSteppedOn(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/Entity;)V"))
+	private void mischief$notSteppedOn(Block block, World world, BlockPos pos, BlockState state, Entity entity, Operation<Void> operation) {
+		if (entity instanceof RatEntity) {
+			return;
+		}
+		operation.call(block, world, pos, state, entity);
+	}
+}
