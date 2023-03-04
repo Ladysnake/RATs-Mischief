1a46c2451685fcce3c33f81e5fbc9e8f63ee13f2
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-03-04 19:19:58 +0000

    masking

--- src/main/java/doctor4t/ratsmischief/client/RatsMischiefClient.java
+++ src/main/java/doctor4t/ratsmischief/client/RatsMischiefClient.java
@@ -1,23 +1,31 @@
 package doctor4t.ratsmischief.client;
 
 import doctor4t.ratsmischief.client.render.entity.RatEntityRenderer;
+import doctor4t.ratsmischief.client.render.entity.RatMasterMaskItemRenderer;
 import doctor4t.ratsmischief.client.render.item.RatItemRenderer;
 import doctor4t.ratsmischief.common.RatsMischief;
 import doctor4t.ratsmischief.common.init.ModEntities;
 import doctor4t.ratsmischief.common.init.ModItems;
 import doctor4t.ratsmischief.common.init.ModParticles;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.model.Dilation;
 import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
+import xyz.amymialee.mialeemisc.MialeeMiscClient;
 
 public class RatsMischiefClient implements ClientModInitializer {
 	public static final EntityModelLayer RAT_MASTER_ARMOR_INNER_LAYER = new EntityModelLayer(RatsMischief.id("rat_master_inner_layer"), "main");
@@ -39,6 +47,16 @@ public class RatsMischiefClient implements ClientModInitializer {
 
 		EntityRendererRegistry.register(ModEntities.RAT, RatEntityRenderer::new);
 		GeoItemRenderer.registerItemRenderer(ModItems.RAT, new RatItemRenderer());
+
+		Identifier itemId = Registry.ITEM.getId(ModItems.RAT_MASTER_MASK);
+		RatMasterMaskItemRenderer inventoryItemRenderer = new RatMasterMaskItemRenderer(itemId);
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(inventoryItemRenderer);
+		BuiltinItemRendererRegistry.INSTANCE.register(ModItems.RAT_MASTER_MASK, inventoryItemRenderer);
+		ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> {
+			out.accept(new ModelIdentifier(itemId, "inventory"));
+			out.accept(new ModelIdentifier(itemId + "_worn", "inventory"));
+		});
+		MialeeMiscClient.INVENTORY_ITEMS.add(ModItems.RAT_MASTER_MASK);
 
 		// model predicates
 		FabricModelPredicateProviderRegistry.register(new Identifier(RatsMischief.MOD_ID + ":filled"), (itemStack, world, livingEntity, seed) -> itemStack.getOrCreateSubNbt(RatsMischief.MOD_ID).getFloat("filled"));
