60593d88a986024fa980c385c786296373d23f5c
doctor4t <25477005+doctor4t@users.noreply.github.com>
2023-03-04 17:21:34 +0100

    Rats don't damage armor

---
+++ src/main/java/doctor4t/ratsmischief/mixin/PlayerEntityMixin.java
@@ -0,0 +1,27 @@
+package doctor4t.ratsmischief.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "damageArmor", at = @At("HEAD"), cancellable = true)
+    public void ratsmischef$removeRatArmorDamage(DamageSource source, float amount, CallbackInfo ci) {
+        if (Objects.equals(source.getName(), "rat")) {
+            ci.cancel();
+        }
+    }
+}
