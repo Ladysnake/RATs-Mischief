f59cba7c1e92d0ea84e4d891d819fe47c2001a6c
unilock <unilock@fennet.rentals>
2024-06-19 12:05:33 -0400

    Fix RAT_CURSE Enchanted Book in Ancient City chests
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/ladysnake/ratsmischief/common/RatsMischief.java
+++ src/main/java/ladysnake/ratsmischief/common/RatsMischief.java
@@ -11,17 +11,7 @@ import ladysnake.ratsmischief.common.init.ModStatusEffects;
 import ladysnake.ratsmischief.common.init.ModTags;
 import ladysnake.ratsmischief.common.world.RatSpawner;
 import ladysnake.ratsmischief.mialeemisc.MialeeMisc;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.entity.decoration.painting.PaintingVariant;
-import net.minecraft.item.EnchantedBookItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTables;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.registry.Registries;
@@ -64,29 +54,6 @@ public class RatsMischief implements ModInitializer {
 
 		// rat kid painting
 		Registry.register(Registries.PAINTING_VARIANT, RatsMischief.id("a_rat_in_time"), new PaintingVariant(64, 48));
-
-		// clothed ingots and rat curse books spawning in ancient city chests
-		UniformLootNumberProvider lootTableRange = UniformLootNumberProvider.create(1, 1);
-		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
-			if (LootTables.ANCIENT_CITY_CHEST.equals(id)) {
-				{ // clothed ingot
-					LootPool lootPool = LootPool.builder()
-						.rolls(lootTableRange)
-						.conditionally(RandomChanceLootCondition.builder(0.3f).build())
-						.with(ItemEntry.builder(ModItems.CLOTHED_INGOT).build()).build();
-					supplier.pool(lootPool);
-				}
-				{ // rat curse book
-					ItemStack book = new ItemStack(Items.ENCHANTED_BOOK);
-					EnchantedBookItem.addEnchantment(book, new EnchantmentLevelEntry(ModEnchantments.RAT_CURSE, 1));
-					LootPool lootPool = LootPool.builder()
-						.rolls(lootTableRange)
-						.conditionally(RandomChanceLootCondition.builder(0.3f).build())
-						.with(ItemEntry.builder(ModItems.CLOTHED_INGOT).build()).build();
-					supplier.pool(lootPool);
-				}
-			}
-		});
 
 		MialeeMisc.onInitialize();
 	}
--- src/main/java/ladysnake/ratsmischief/common/init/ModLootTables.java
+++ src/main/java/ladysnake/ratsmischief/common/init/ModLootTables.java
@@ -1,42 +1,38 @@
 package ladysnake.ratsmischief.common.init;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.enchantment.EnchantmentLevelEntry;
-import net.minecraft.item.EnchantedBookItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.LootFunction;
-import net.minecraft.loot.function.LootFunctionType;
+import net.minecraft.loot.function.SetEnchantmentsLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.LootNumberProvider;
 
 public class ModLootTables {
-	public static final float RAT_CURSE_CHANCE = 0.2f; // 20%
+	private static final float CLOTHED_INGOT_CHANCE = 0.3f; // 30%
+	private static final float RAT_CURSE_CHANCE = 0.2f; // 20%
+	private static final LootNumberProvider CONST_1 = ConstantLootNumberProvider.create(1);
 
 	public static void initialize() {
-		LootTableEvents.MODIFY.register((resourceManager, lootManager, identifier, fabricLootSupplierBuilder, lootTableSetter) -> {
-			if (LootTables.ANCIENT_CITY_CHEST.equals(identifier)) {
-				fabricLootSupplierBuilder.pool(new LootPool.Builder()
-					.rolls(ConstantLootNumberProvider.create(1))
-					.with(ItemEntry.builder(Items.BOOK).apply(() -> new LootFunction() {
-						@Override
-						public LootFunctionType getType() {
-							throw new UnsupportedOperationException();
-						}
-
-						@Override
-						public ItemStack apply(ItemStack itemStack, LootContext lootContext) {
-							EnchantmentLevelEntry enchantment = new EnchantmentLevelEntry(ModEnchantments.RAT_CURSE, 1);
-							return EnchantedBookItem.forEnchantment(enchantment);
-						}
-					}).build())
-					.conditionally(RandomChanceLootCondition.builder(RAT_CURSE_CHANCE).build())
-					.build()
-				);
+		// clothed ingots and rat curse books spawning in ancient city chests
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
+			if (LootTables.ANCIENT_CITY_CHEST.equals(id)) {
+				{ // clothed ingot
+					LootPool lootPool = LootPool.builder()
+						.rolls(CONST_1)
+						.conditionally(RandomChanceLootCondition.builder(CLOTHED_INGOT_CHANCE).build())
+						.with(ItemEntry.builder(ModItems.CLOTHED_INGOT).build()).build();
+					supplier.pool(lootPool);
+				}
+				{ // rat curse book
+					LootPool lootPool = LootPool.builder()
+						.rolls(CONST_1)
+						.conditionally(RandomChanceLootCondition.builder(RAT_CURSE_CHANCE).build())
+						.with(ItemEntry.builder(Items.ENCHANTED_BOOK).apply((new SetEnchantmentsLootFunction.Builder()).enchantment(ModEnchantments.RAT_CURSE, CONST_1)).build()).build();
+					supplier.pool(lootPool);
+				}
 			}
 		});
 	}
